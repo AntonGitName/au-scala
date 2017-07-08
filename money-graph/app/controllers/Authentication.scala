395bd51688dca74fd9572bb809a2cabf606074c1
Anton Mordberg <mordbergak@gmail.com>
2017-07-08 20:05:09 +0300

    Register form added. TODO: fix logs

--- money-graph/app/controllers/Authentication.scala
+++ money-graph/app/controllers/Authentication.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import models.{Account, AccountDao}
+import play.api.Logger
 import play.api.data.Forms._
 import play.api.data._
 import play.api.mvc._
@@ -10,11 +11,13 @@ import views._
 
 class Authentication @Inject() (cc: ControllerComponents, dao : AccountDao) extends AbstractController(cc) {
 
+    val logger: Logger = Logger(this.getClass())
+
     val loginForm = Form(
         tuple(
             "email" -> email,
             "password" -> nonEmptyText
-        ) verifying ("Invalid email or password", {
+        ) verifying ("Invalid email or password", result => result match {
             case (mail, password) => dao.authenticate(mail, password).isDefined
         })
     )
@@ -30,7 +33,7 @@ class Authentication @Inject() (cc: ControllerComponents, dao : AccountDao) exte
       * Logout and clean the session.
       */
     def logout = Action {
-        Redirect(routes.Authentication.login()).withNewSession.flashing(
+        Redirect(routes.Authentication.login).withNewSession.flashing(
             "success" -> "You've been logged out"
         )
     }
@@ -42,6 +45,40 @@ class Authentication @Inject() (cc: ControllerComponents, dao : AccountDao) exte
         loginForm.bindFromRequest.fold(
             formWithErrors => BadRequest(html.login(formWithErrors)),
             account => Redirect(routes.Restricted.index()).withSession("email" -> account._1)
+        )
+    }
+
+    val registerForm = Form(
+        tuple(
+            "email" -> email,
+            "name" -> nonEmptyText,
+            "password" -> nonEmptyText
+        ) verifying ("Account with specified email already exists", result => result match {
+            case (mail, name, password) => dao.findByEmail(mail).isEmpty
+        })
+    )
+
+    /**
+      * Register page.
+      */
+    def register = Action { implicit request =>
+        Ok(html.register(registerForm))
+    }
+
+    /**
+      * Handle register form submission.
+      */
+    def createAccount = Action { implicit request =>
+        registerForm.bindFromRequest.fold(
+            formWithErrors => {
+                logger.debug("Account creation aborted")
+                BadRequest(html.register(formWithErrors))
+            },
+            account => {
+                logger.debug("Creating new account for " + account._1)
+                dao.create(Account(account._1, account._2, account._3))
+                Redirect(routes.Restricted.index()).withSession("email" -> account._1)
+            }
         )
     }
 
